893fcba8cb5c2d3c9946dde314ec4af6299ec04d
Anuj M <61815166+zAzNzUzJ@users.noreply.github.com>
2024-08-18 13:26:20 +0530

    Update setup.sh
    Simple script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
+# note - Only in Centos7 
+#its hardcoded edit as per requirement
 
-
-echo "iNSTALLAION "
+echo "####    iNSTALLAION   ####"
+# mirror repo problem in centos7
 
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 
 sed -i 's/#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
-
+#small function just to check the command is executed or not
 cc(){
 if [ $? -ne 0 ];then
-echo "Failed the command !!!!"
+echo "##########     Failed the command !!!!    ########### "
 exit
 else
  echo "	#########	DONE - WORKING 	##########"
@@ -38,15 +40,15 @@ cc
 systemctl enable xinetd
 cc
 
-#cp dhcp.back dhcp.cf
+cp dhcp.back dhcp.cf
 cc
 
-#bash -x dhcp.sh
+bash -x dhcp.sh # dhcp script will run  replaces all the values as per the user input
 cc
 
-#cp dhcp.cf /etc/dhcp/dhcpd.conf
+cp dhcp.cf /etc/dhcp/dhcpd.conf
 cc
-#systemctl restart dhcpd
+systemctl restart dhcpd
 cc
 
 mkdir -p /var/pxe/centos7 
@@ -54,23 +56,23 @@ cc
 mkdir -p /var/lib/tftpboot/centos7/root
 cc
 
-#yum -y install dracut-network
+#yum -y install dracut-network  #not required 
 cc
-yum -y install nfs-utils 
+yum -y install nfs-utils  # here we use nfs for stateless provisioning
 cc
-#yum groups -y install "Minimal Install" --releasever=7 --installroot=/var/lib/tftpboot/centos7/root/
+
+#change the installastion as per required GUI or minimal 
+yum groups -y install "Minimal Install" --releasever=7 --installroot=/var/lib/tftpboot/centos7/root/ # install the rool directory full files system required for the boot in the given path
 cc
 echo  "Enter the password for the root in client side..!!! "
 bash password.sh
 cc
 mv /home/admin/Desktop/script-stateless/shadow.temp /var/lib/tftpboot/centos7/root/etc/shadow
 cc
-cp fstab.cfg /var/lib/tftpboot/centos7/root/etc/fstab
+cp fstab.cfg /var/lib/tftpboot/centos7/root/etc/fstab  # add the nfs dir path in for the clientt to get dir access
 cc
 
-
-
-read -p "ente rthe location of the iso file " loc_ISO
+#read -p "Enter the location of the iso file " loc_ISO
 #mount -t iso9660 -o loop $loc_ISO  /var/pxe/centos7
 cc
 
@@ -89,7 +91,7 @@ sed -i "s/sysip/$ipa/g" /home/admin/Desktop/script-stateless/default.menu
 cc
 cp /home/admin/Desktop/script-stateless/default.menu /var/lib/tftpboot/pxelinux.cfg/default
 cc
-echo "/var/lib/tftpboot/centos7/root $ip_add/24(rw,no_root_squash)" > /etc/exports
+echo "/var/lib/tftpboot/centos7/root $ipa/24(rw,no_root_squash)" > /etc/exports
 
 systemctl start rpcbind nfs-server
 systemctl enable rpcbind nfs-server
@@ -98,8 +100,3 @@ systemctl enable rpcbind nfs-server
 #cp pxeboot.config /etc/httpd/conf.d/pxeboot.conf
 cc
 #systemctl restart httpd 
-
-
-
-
-
