6056a686d4026ce8b8a1e3c92ac344b0256f5407
Anuj M <61815166+zAzNzUzJ@users.noreply.github.com>
2024-08-18 13:25:05 +0530

    Update dhcp.sh
    SIMPLE SCRIPT

--- dhcp.sh
+++ dhcp.sh
@@ -1,3 +1,7 @@
+#just simple replace as per the file in dhcp.back
+# replacing it with the respective variable or key words used in that hardcoded
+
+
 echo "test"
 
 read -p "Enter the Hostonly IP " hostip
@@ -9,17 +13,19 @@ sed -i "s/start_ip/$stip/g" dhcp.cf
 read -p "enter the End range of the ip : " endip
 sed -i "s/endip/$endip/g" dhcp.cf
 
-read -p "Enter the Gateway ip : " gatewayip
+read -p "Enter the Gateway ip : (master host IP) " gatewayip
 sed -i "s/gatewayip/$gatewayip/g" dhcp.cf
 
 read -p "network : " ip_network
-read -p "network mask : " network_mask
+read -p "network mask :(255.255.255.0) " network_mask
 
 sed -i "s/ip_network/$ip_network/g" dhcp.cf
 sed -i "s/network_mask/$network_mask/g" dhcp.cf
 
-read -p "Enter the Broadcast IP : " broadcastip
+read -p "Enter the Broadcast IP : (end octant .255)" broadcastip
 sed -i "s/broadcastip/$broadcastip/g" dhcp.cf
+
+
 
 
 
